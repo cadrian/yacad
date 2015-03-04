a81456cd1ce5979289631bf1549aa23de4291d09
Cyril Adrian <cyril.adrian@gmail.com>
2015-03-04 11:38:08 +0100

    draft tasklist

---
+++ src/tasklist/yacad_tasklist.c
@@ -0,0 +1,78 @@
+/*
+  This file is part of yaCAD.
+
+  yaCAD is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, version 3 of the License.
+
+  yaCAD is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with yaCAD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cad_array.h>
+#include <cad_hash.h>
+
+#include "yacad_tasklist.h"
+
+typedef struct yacad_tasklist_impl_s {
+     yacad_tasklist_t fn;
+     const char *database_name;
+     cad_hash_t *tasklist_per_runner;
+} yacad_tasklist_impl_t;
+
+static void add(yacad_tasklist_impl_t *this, yacad_task_t *task) {
+     const char *runner_name = task->get_runner_name(task);
+     cad_array_t *tasklist = this->tasklist_per_runner->get(this->tasklist_per_runner, runner_name);
+     if (tasklist == NULL) {
+          tasklist = cad_new_array(stdlib_memory);
+          this->tasklist_per_runner->set(this->tasklist_per_runner, runner_name, tasklist);
+     }
+     tasklist->insert(tasklist, tasklist->count(tasklist), task);
+     // TODO save to database
+}
+
+static yacad_task_t *get(yacad_tasklist_impl_t *this, const char *runner_name) {
+     yacad_task_t *result = NULL;
+     cad_array_t *tasklist = this->tasklist_per_runner->get(this->tasklist_per_runner, runner_name);
+     if (tasklist != NULL) {
+          result = tasklist->del(tasklist, 0);
+     }
+     // TODO save to database // NO!! do that only when the task is successfully performed (we need an extra method here)
+     return result;
+}
+
+static void free_tasklist(cad_hash_t *dict, int index, const char *runner_name, cad_array_t *tasklist, yacad_tasklist_impl_t *this) {
+     int i, n = tasklist->count(tasklist);
+     yacad_task_t *task;
+     for (i = 0; i < n; i++) {
+          task = tasklist->get(tasklist, i);
+          task->free(task);
+     }
+     tasklist->free(tasklist);
+}
+
+static void free_(yacad_tasklist_impl_t *this) {
+     this->tasklist_per_runner->clean(this->tasklist_per_runner, (cad_hash_iterator_fn)free_tasklist, this);
+     this->tasklist_per_runner->free(this->tasklist_per_runner);
+     free(this);
+}
+
+static yacad_tasklist_t impl_fn = {
+     .add = (yacad_tasklist_add_fn)add,
+     .get = (yacad_tasklist_get_fn)get,
+     .free = (yacad_tasklist_free_fn)free_,
+};
+
+yacad_tasklist_t *yacad_tasklist_new(const char *database_name) {
+     yacad_tasklist_impl_t *result = malloc(sizeof(yacad_tasklist_impl_t));
+     result->fn = impl_fn;
+     result->database_name = database_name;
+     result->tasklist_per_runner = cad_new_hash(stdlib_memory, cad_hash_strings);
+     // TODO read from database
+     return &(result->fn);
+}
