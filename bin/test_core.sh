3e5db454afe27a76840507b5c618c5fa729e0870
Cyril Adrian <cyril.adrian@gmail.com>
2015-03-11 11:00:34 +0100

    better test

--- bin/test_core.sh
+++ bin/test_core.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cd $(dirname $(readlink -f $0))/../test
+
+sed 's!#PATH#!'"$(cd ..; pwd)"'!g' core.conf.template > core.conf
+
 if [ ${gdb-no} = yes ]; then
     exec gdb ../target/yacad_core --args ../target/yacad_core "$@"
 elif [ ${strace-no} = yes ]; then
     strace ../target/yacad_core "$@"
+elif [ ${valgrind-no} = yes ]; then
+    valgrind ../target/yacad_core "$@"
 fi
 exec ../target/yacad_core "$@"
