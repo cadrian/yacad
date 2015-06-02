300e15c3fbfebcf504ba47aebc73aefdccb5a6c0
Cyril Adrian <cyril.adrian@gmail.com>
2015-06-02 10:57:45 +0200

    todo

---
+++ test/unit/test_database_sqlite3.c
@@ -0,0 +1,26 @@
+/*
+  This file is part of yaCAD.
+
+  yaCAD is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, version 3 of the License.
+
+  yaCAD is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with yaCAD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "test.h"
+
+#include "common/database/yacad_database_sqlite3.h"
+
+int test(void) {
+     int result = 0;
+     static logger_t log = get_logger(info);
+
+     return result;
+}
